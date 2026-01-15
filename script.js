469a5e1f540acbc3f5e2adbec61515aa8bb7088a
clkaha <claire.hagger@norfolk.gov.uk>
2026-01-15 16:31:00 +0000

    Added links to github and netlify.

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
+function displayTemperature(response) {
+	let temperatureElement = document.querySelector("#current-temperature");
+	let temperature = Math.round(response.data.temperature.current);
+	let cityElement = document.querySelector("#current-city");
+	cityElement.innerHTML = response.data.city;
+	temperatureElement.innerHTML = temperature;
+}
+
 function search(event) {
 	event.preventDefault();
 	let searchInputElement = document.querySelector("#search-input");
-	let cityElement = document.querySelector("#current-city");
-	cityElement.innerHTML = searchInputElement.value;
-	function currentTemperature(response) {
-		let currentWeatherIconElement = document.querySelector(
-			".current-temperature-icon"
-		);
-		let currentTemperatureElement = document.querySelector(
-			".current-temperature-value"
-		);
-		let currentWeatherIcon = response.data.condition.icon_url;
-		let currentTemperatureValue = Math.round(response.data.temperature.current);
-		currentWeatherIconElement.innerHTML = `<img src="${currentWeatherIcon}" alt="" />`;
-		currentTemperatureElement.innerHTML = `${currentTemperatureValue}`;
-	}
 	let city = searchInputElement.value;
-	let apiKey = "t892ofdde3d43fb03b089a7dffb097a9";
+
+	let apiKey = "b2a5adcct04b33178913oc335f405433";
 	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-	axios.get(apiUrl).then(currentTemperature);
+
+	axios.get(apiUrl).then(displayTemperature);
 }
 
 function formatDate(date) {
